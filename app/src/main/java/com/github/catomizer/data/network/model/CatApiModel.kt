5f0946b80430d5ce2881761b3884223bce65fa81
Alexey Pushkarev <alexey_pushkarev1@epam.com>
2019-12-30 11:20:52 +0300

    code clean up: left todo comment

--- app/src/main/java/com/github/catomizer/data/network/model/CatApiModel.kt
+++ app/src/main/java/com/github/catomizer/data/network/model/CatApiModel.kt
@@ -1,3 +1,4 @@
 package com.github.catomizer.data.network.model
 
+//TODO: It would be better to separate api model and ui model, but they will be the same. That's why I leave it as it for now.
 data class CatApiModel(val id: String, val url: String)
